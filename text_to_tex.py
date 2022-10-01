eeed3229288deafcb8aff2759bd71122017691c7
DC23 <jugglindan@gmail.com>
2022-10-01 23:14:57 +1000

    resolves #9

--- text_to_tex.py
+++ text_to_tex.py
@@ -10,7 +10,6 @@ def emit_table_header(f, name):
     f.write(TABLE_HEADER.format(table_name=name))
 
 
-# $1$ &  The temporal conflict between \st{Faction} and \st{Faction} brought on by the
 def emit_table_row(f, line: str, number: int = 1):
     line = line.replace("[", r"\subtable{").replace("]", "}")
     f.write(f"    {number} & {line} \\\\\n")
@@ -27,21 +26,28 @@ if __name__ == "__main__":
     output_directory = Path("./tex")
     output_directory.mkdir(exist_ok=True)
 
-    for input_path in Path("./text/").glob("*.txt"):
-        # create the output file
-        output_path = Path.joinpath(
-            output_directory, Path(input_path.name).with_suffix(".tex")
-        )
-        with open(output_path, "w") as output_file:
-            with open(input_path) as input_file:
-                # get the table name from the first line
-                table_name = input_file.readline().strip()
-                emit_table_header(output_file, table_name)
+    main_include_path = Path.joinpath(output_directory, "all_tables.tex")
 
-                # render table rows from each of the remaining lines
-                row_number = 1
-                for line in input_file:
-                    emit_table_row(output_file, line.strip(), row_number)
-                    row_number += 1
+    with open(main_include_path, "w") as main_include:
+        for input_path in Path("./text/").glob("*.txt"):
+            # create the output file
+            output_path = Path.joinpath(
+                output_directory, Path(input_path.name).with_suffix(".tex")
+            )
 
-            emit_table_footer(output_file)
+            # add this file to the main tex include file
+            main_include.write(f"\\input{{{output_path}}}\n")
+
+            with open(output_path, "w") as output_file:
+                with open(input_path) as input_file:
+                    # get the table name from the first line
+                    table_name = input_file.readline().strip()
+                    emit_table_header(output_file, table_name)
+
+                    # render table rows from each of the remaining lines
+                    row_number = 1
+                    for line in input_file:
+                        emit_table_row(output_file, line.strip(), row_number)
+                        row_number += 1
+
+                emit_table_footer(output_file)
